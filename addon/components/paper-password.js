adeeac3b8982179af542f65cf5286c5c4b8b735f
Dan Hill <mail@danhill.us>
2017-12-21 02:13:49 -0600

    Add in field icons.

--- addon/components/paper-password.js
+++ addon/components/paper-password.js
@@ -6,6 +6,9 @@ const { Component, computed, A, assert } = Ember;
 
 export default Ember.Component.extend({
   layout,
+  classNames: ['paper-password-container'],
+  showPassword: false,
+  type: 'password',
   minStrength: 3,
   strengthLabel: 'Password strength: ',
   strengthLevels: ['Very Poor', 'Poor', 'Fair', 'Good', 'Excellent'],
@@ -30,7 +33,15 @@ export default Ember.Component.extend({
   strengthWarning: computed('value', function() {
     return this.get('passwordStrength').score < this.get('minStrength');
   }),
-
+  'colorClass': computed('passwordStrength.score','value', 'minStrength', function() {
+    if(this.get('passwordStrength').score >= this.get('minStrength') && this.get('passwordStrength').score < 4){
+      return 'md-primary md-hue-4';
+    } else if (this.get('passwordStrength').score < this.get('minStrength')){
+      return 'md-warn md-hue-4';
+    } else if (this.get('passwordStrength').score >= 4){
+      return 'md-accent md-hue-1'
+    }
+  }),
   inputErrors: computed('errors.[]', 'value', function() {
     let myErrors = A().pushObjects(this.get('errors'));
     let passwordStrength = this.get('passwordStrength');
@@ -48,5 +59,15 @@ export default Ember.Component.extend({
   didReceiveAttrs() {
     this._super(...arguments);
     assert('{{paper-password}} requires an `onChange` action or null for no action.', this.get('onChange') !== undefined);
+  },
+  actions: {
+    'show-password': function(){
+      this.set('showPassword', true)
+      this.set('type', 'text')
+    },
+    'hide-password': function(){
+      this.set('showPassword', false)
+      this.set('type', 'password')
+    }
   }
 });
